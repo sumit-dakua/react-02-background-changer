51adb3f59b7dfefecbc5536931eaa2e87b1eea6b
sumit-dakua <sumitdakua4@gmail.com>
2024-05-25 02:02:47 +0530

    fixed app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import ColorButton from './components/colorButton.jsx'
 
 function App() {
-  const [color, setColor] = useState("black")
+  const [color, setColor] = useState("lavender")
 
   const colors= [
     { colorName: 'Red', color: 'red' },
@@ -30,7 +30,7 @@ function App() {
             ))}
 
             <button
-              onClick={() => setColor("")}
+              onClick={() => setColor("lavender")}
               className="outline-none px-6 py-2 rounded-full text-black font-bold border-2 border-gray-300"
               style={{backgroundColor: "white", boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.1)"}}
             > Reset </button>
